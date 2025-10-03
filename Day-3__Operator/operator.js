4951948d3add50fe47a145fee71ef03230bcc035
Dabhi Pratik <pratikdabhi4902@gmail.com>
2025-10-03 11:16:11 +0530

    added

---
+++ Day-3__Operator/operator.js
@@ -0,0 +1,52 @@
+/*
+JavaScript operators are special symbols that perform operations on one or more operands (values). For example:
+
+2 + 4;  // 6
+
+Here, we used the `+` operator to add the operands 2 and 4.
+*/
+
+// There are a total of seven types of operators in JavaScript:
+
+// 1. Arithmetic Operators
+// 2. Assignment Operators
+// 3. Comparison Operators
+// 4. Logical Operators
+// 5. Bitwise Operators
+// 6. Unary Operators
+// 7. Miscellaneous Operators
+
+// ---------------------------------------------
+
+// 1. Arithmetic Operators
+// Used to perform mathematical operations.
+
+let a = 20
+let b = 10
+
+console.log("Addition: ",a+b);
+console.log("Subtraction: ",a-b);
+console.log("Multilication: ",a*b);
+console.log("Division:", a/b); 
+console.log("Modulus:", a % b); 
+console.log("Exponentiation:", a ** b);
+
+// ---------------------------------------------
+
+// 2. Assignment Operators
+// Used to assign values to variables.
+
+let x = 3
+
+x += 4 // x = x+4
+
+console.log("x after x+=4 : ",x);
+
+
+// ---------------------------------------------
+
+// 3. Comparison Operators
+// Used to compare two values and return a boolean result.
+
+
+
