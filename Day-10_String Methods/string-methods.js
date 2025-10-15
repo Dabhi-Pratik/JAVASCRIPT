2fa94e241ca66d555e915bc9db80c8cbe051e795
Dabhi Pratik <pratikdabhi4902@gmail.com>
2025-10-15 11:25:21 +0530

    string methods

---
+++ Day-10_String Methods/string-methods.js
@@ -0,0 +1,7 @@
+// more strings methods
+
+let text ='Hello Pratik'
+
+// match() method ==> searches a string for a value, or a regular expression, and returns the matches
+
+console.log("(Match Method) Finding match in String: ",text.match("ra"))
