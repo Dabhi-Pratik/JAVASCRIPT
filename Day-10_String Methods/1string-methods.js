86a794a9d955226f4878fc1bdb0d9ea06fd0e72f
Dabhi Pratik <pratikdabhi4902@gmail.com>
2025-10-16 12:09:01 +0530

    string--methods

---
+++ Day-10_String Methods/1string-methods.js
@@ -0,0 +1,61 @@
+let text = 'Hello Pratik'
+
+//  split() methods ==> Splits a string into an array of substrings
+
+console.log("Split String into Array with Space Added: ",text.split(" "))
+
+console.log("Split String into Array without Space Added: ",text.split(" "))
+
+console.log("Split String with o",text.split("o")) // will remove specified value in text and return string
+
+// startsWith() methods ==> startswith method whether a string begins with specified characters
+
+console.log("startswith method: ",text.startsWith("Hello"))
+
+console.log("startswith method: ",text.startsWith("Pr"))
+
+// The substring() method extracts characters, between two indices (positions), from a string, and returns the substring.
+
+// The substring() method extracts characters from start to end (exclusive).
+
+// The substring() method does not change the original string.
+
+// If start is greater than end, arguments are swapped: (4, 1) = (1, 4).
+
+// Start or end values less than 0, are treated as 0
+
+console.log("Text: ",text)
+
+console.log("Substring Example: ",text.substring(6))
+
+console.log("Substring Op1: ",text.substring(2,8))
+
+console.log("Substring Op2: ",text.substring(-1)) // return full text
+
+// tolowerCase() ==> toLowerCase()	Returns a string converted to lowercase letters
+
+console.log("Lower case op => ",text.toLowerCase());
+
+// toupperCase() ==> Returns a string converted to uppercase letters
+
+console.log("Upper Case OP => ",text.toUpperCase());
+
+// trim()	Returns a string with removed whitespace
+
+let trimTextexample ="        Hello             ";
+
+console.log("Length OP text for trim => ",trimTextexample.length)
+
+console.log("white-Space removed and then after length of string => ",trimTextexample.trim().length)
+
+// we can trim from start and end using trim.start() and trim.end() methods
+
+// example
+
+console.log("removing white-Space from starting part only and then after length of string => ",trimTextexample.trimStart().length)
+
+// trim.end() example
+
+console.log("removing white-Space from starting part only and then after length of string => ",trimTextexample.trimEnd().length)
+
+
--- Day-10_String Methods/string-methods.js
+++ Day-10_String Methods/string-methods.js
@@ -1,7 +1,45 @@
 // more strings methods
 
-let text ='Hello Pratik'
+let text = 'Hello Pratik'
 
 // match() method ==> searches a string for a value, or a regular expression, and returns the matches
 
-console.log("(Match Method) Finding match in String: ",text.match("ra"))
+console.log("(Match Method) Finding match in String: ", text.match("ra"))
+
+// if not finding  matching string it will return null
+
+console.log("Not finding Match in string : ", text.match("ab"))
+
+// repeat() method ==> returns new string with a number of copies of strings
+
+console.log("Repeat the string: ", text.repeat(2))
+
+// replace()  method ==> searches a string for a pattern and return a new string where First match are replaced
+
+console.log("Replacing Hello to Dabhi: ",text.replace("Hello","Dabhi"))
+
+// replaceAll() method ==> searches a string for a pattern and returns a new string where all matches are replaced
+
+console.log("Replacing l to L in all syntax: ",text.replaceAll("l","L"))
+
+// search() method ==> searches a string for a value, or regular expression, and returns  the index(position) of the match
+
+console.log("Searching l in text: ",text.search("l"))
+
+//here we cant put starting index we can do that indexOf method
+
+console.log("indexOf op with starting index =>",text.indexOf("P",2))
+
+// slice() method ==> extracts a part of a string and returns a new string
+
+// here first is staring index and after that end index is start counting from 0
+
+console.log("Text Example: ",text)
+
+console.log("Slice OP: ",text.slice(3))
+
+console.log("Slice OP: ",text.slice(2,5))
+
+console.log("Slice OP: ",text.slice(3,8))
+
+console.log("Slice OP: ",text.slice(-1))  // it will start from last
