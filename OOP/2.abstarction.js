6cee2ac542a467422df92700b5c55c5a0cb1169a
Dabhi Pratik <pratikdabhi4902@gmail.com>
2025-11-22 10:58:46 +0530

    oop

---
+++ OOP/2.abstarction.js
@@ -0,0 +1,62 @@
+// **Abstraction** => Abstraction means hiding certain details that don't matter to the user and only showing essential features or functions.
+// Example of this:
+// FrontCamOn(), FrontCamOff() => front Camera
+
+//Class representing a Car
+
+class Car{
+    //Private property (not directly accessible)
+    #engineStatus ="off";
+
+    //Method to turn the engine on
+    startEngine(){
+        this.#engineStatus = "on";
+        console.log("Engine Started");
+    }
+
+    //Method to turn the engine off
+    stopEngine(){
+        this.#engineStatus = "off";
+        console.log("Engine Stopped");
+    }
+
+    // Public method to get engine status (Abstraction: hides internal details)
+    getEngineStatus(){
+        return this.#engineStatus;
+    }
+}
+
+//creating a car object;
+
+const myCar=new Car();
+
+// Using abstraction to interact with the car without worrying about the engine's internal details
+myCar.startEngine();
+console.log(myCar.getEngineStatus()) // on
+myCar.stopEngine();
+console.log(myCar.getEngineStatus()); //off
+
+// another example  light on off
+
+class TubLight{
+    #lightStatus ="on";
+
+    startLight(){
+        this.#lightStatus ="on";
+        console.log("Light is on");
+    }
+
+    turnOffLight(){
+        this.#lightStatus ="off";
+        console.log("Light is OFF");
+    }
+
+    getLightStatus(){
+        return this.#lightStatus();
+    }
+}
+
+const light = new TubLight()
+
+light.startLight();
+light.turnOffLight();
